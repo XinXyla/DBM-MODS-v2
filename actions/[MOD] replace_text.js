e5baa4f10ca3f9eef31f3f07a286294bb6a5581d
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-01-27 14:24:24 -0300

    Update [MOD] replace_text.js

--- actions/[MOD] replace_text.js
+++ actions/[MOD] replace_text.js
@@ -1,100 +1,86 @@
 module.exports = {
-
-	name: "Replace Text",
-	
-	section: "Other Stuff",
-	
+	name: 'Replace Text',
+	section: 'Other Stuff',
+	meta: {
+	  version: '2.1',
+	  preciseCheck: false,
+	  author: 'DBM Mods + XinXla',
+	  downloadURL: 'https://github.com/XinXyla/DBM-MODS-v2/blob/main/actions/%5BMOD%5D%20replace_text.js',
+	},
+  
 	subtitle: function(data) {
 		const info = ['Substitua o primeiro resultado', 'Substitua todos os resultados']
 		return `${info[data.info]} "${data.text2}" por "${data.text3}" em "${data.text}"`;
 	},
-		
-	author: "XinXyla",
-	
-	version: "1.0",
-	
-	short_description: "Substitua o texto por outro texto",
-	
-	variableStorage: function (data, varType) {
-		const type = parseInt(data.storage);
-		if (type !== varType) return;
-		let dataType = 'String';
-		return ([data.varName, dataType]);
+  
+	variableStorage(data, varType) {
+	  if (parseInt(data.storage, 10) !== varType) return;
+	  return [data.varName, 'String'];
 	},
-	
+  
 	fields: ["text", "text2", "text3", "info", "storage", "varName"],
-	
-	html: function(isEvent, data) {
-		return `
-	<div style="padding-top: 3px;">
-	Texto original:
-		<textarea id="text" rows="5" placeholder="Insira o texto aqui..." style="width: 99%; font-family: monospace; white-space: nowrap;"></textarea>
-	</div>
-	<div>
-		<div style="padding-top: 8px; width: 100%;">
-		Substituir:<br>
-			<textarea id="text2" rows="1" class="round" style="width:100%;"></textarea>
-		</div><div style=" padding-top: 8px; width: 100%;">
-			Para isso:<br>
-			<textarea id="text3" rows="1" class="round" style="width:100%"></textarea>
-		</div>
-	</div>
-	<div>	
-	Modelo:<br>
-	<select id="info" class="round">
-		<option value="0" selected>Substitua o primeiro resultado</option>
-		<option value="1">Substitua todos os resultados</option>
-	</select>
-	</div>
-	<div style="padding-top: 8px;">
-		<div style="float: left; width: 35%;">
-		Armazenar em:<br>
-			<select id="storage" class="round">
-				${data.variables[1]}
-			</select>
-		</div>
-		<div id="varNameContainer" style="float: right; width: 60%;">
-			Nome da variável:<br>
-			<input id="varName" class="round" type="text">
-		</div>
-	</div>
-		`
+  
+	html(isEvent, data) {
+	  return `
+	  <div style="padding-top: 3px;">
+	  Texto original:
+		  <textarea id="text" rows="5" placeholder="Insira o texto aqui..." style="width: 99%; font-family: monospace; white-space: nowrap;"></textarea>
+	  </div>
+	  <div>
+		  <div style="padding-top: 8px; width: 100%;">
+		  Substituir:<br>
+			  <textarea id="text2" rows="1" class="round" style="width:100%;"></textarea>
+		  </div><div style=" padding-top: 8px; width: 100%;">
+			  Para isso:<br>
+			  <textarea id="text3" rows="1" class="round" style="width:100%"></textarea>
+		  </div>
+	  </div>
+	  <div>	
+	  Modelo:<br>
+	  <select id="info" class="round">
+		  <option value="0" selected>Substitua o primeiro resultado</option>
+		  <option value="1">Substitua todos os resultados</option>
+	  </select>
+	  </div>
+	  <div style="padding-top: 8px;">
+		  <div style="float: left; width: 35%;">
+		  Armazenar em:<br>
+			  <select id="storage" class="round">
+				  ${data.variables[1]}
+			  </select>
+		  </div>
+		  <div id="varNameContainer" style="float: right; width: 60%;">
+			  Nome da variável:<br>
+			  <input id="varName" class="round" type="text">
+		  </div>
+	  </div>`;
 	},
-	
-	init: function() {
-		},
-	
-	action: function(cache) {
-		const data = cache.actions[cache.index];
-		const storage = parseInt(data.storage);
+  
+	init() {},
+  
+	async action(cache) {
+	  const data = cache.actions[cache.index];
+	  const text = this.evalMessage(data.text, cache);
+	  const oldValue = this.evalMessage(data.text2, cache);
+	  const newValue = this.evalMessage(data.text3, cache);
+	  const info = parseInt(data.info, 10);
+  
+	  let result;
+	  switch (info) {
+		case 0:
+		  result = text.replace(oldValue, newValue);
+		  break;
+		case 1:
+		  result = text.split(oldValue).join(newValue);
+		  break;
+	  }
+	  if (result !== undefined) {
+		const storage = parseInt(data.storage, 10);
 		const varName = this.evalMessage(data.varName, cache);
-		const text = this.evalMessage(data.text, cache);
-		const text2 = this.evalMessage(data.text2, cache);
-		const text3 = this.evalMessage(data.text3, cache);
-		const info = parseInt(data.info);
-	
-		let result;
-		switch(info) {
-			case 0:
-				result = text.replace(text2, text3);
-				break;
-			case 1:
-				const WrexMODS = this.getWrexMods();
-				const replacestr = WrexMODS.require('replace-string');
-				result = replacestr(text, text2, text3);
-				break;
-			default:
-				break;
-		}
-		if (result !== undefined) {
-			const storage = parseInt(data.storage);
-			const varName = this.evalMessage(data.varName, cache);
-			this.storeValue(result, storage, varName, cache);
-		}
-		this.callNextAction(cache);
+		this.storeValue(result, storage, varName, cache);
+	  }
+	  this.callNextAction(cache);
 	},
-	
-	mod: function(DBM) {
-	}
-	
-	};
+  
+	mod() {},
+  };
